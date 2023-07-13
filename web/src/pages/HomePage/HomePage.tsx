58eaa8a044a1f8bf45d6d4501716850e580206cf
David Thyresson <dthyresson@gmail.com>
2023-07-13 11:53:55 -0400

    Make a button

--- web/src/pages/HomePage/HomePage.tsx
+++ web/src/pages/HomePage/HomePage.tsx
@@ -2,20 +2,27 @@ import { MetaTags } from '@redwoodjs/web'
 
 const HomePage = () => {
   return (
-    <>
+    <div className="p-12">
       <MetaTags title="Home" description="Home page" />
 
-      <h1 className="text-2xl">Defer + RedwoodJS Example</h1>
-      <p>
-        <button
-          onClick={() =>
-            fetch(`${globalThis.RWJS_API_URL}/runHelloWorld?name=dt`)
-          }
-        >
-          Hello!
-        </button>
-      </p>
-    </>
+      <h1 className="mb-6 text-2xl">Defer + RedwoodJS Example</h1>
+      <div className="flex">
+        <div>
+          <button
+            className="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            onClick={async () => {
+              const response = await fetch(
+                `${globalThis.RWJS_API_URL}/runHelloWorld?name=dt`
+              )
+
+              alert((await response.json()).data)
+            }}
+          >
+            Hello!
+          </button>
+        </div>
+      </div>
+    </div>
   )
 }
 
